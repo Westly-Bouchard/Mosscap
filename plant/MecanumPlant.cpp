dab6fac546896a6e041b5ec22becc3957eb99d1d
West <westly.bouchard@gmail.com>
2026-05-06 17:26:18 -0600

    Fix coordinate system to align a theta of zero with the positive x-axis

--- plant/MecanumPlant.cpp
+++ plant/MecanumPlant.cpp
@@ -12,10 +12,10 @@ MecanumPlant::MecanumPlant(const MecanumConfig& config) : config(config) {}
 
 void MecanumPlant::operator()(const state_t &x, state_t &dxdt, double t) const {
     // Calculate forces in the body frame
-    const double bFy = (inputs.at(0) + inputs.at(1) + inputs.at(2) +
+    const double bFx = (inputs.at(0) + inputs.at(1) + inputs.at(2) +
                         inputs.at(3)) / config.wheelRadius;
 
-    const double bFx = (-inputs.at(0) + inputs.at(1) + inputs.at(2) -
+    const double bFy = (-inputs.at(0) + inputs.at(1) + inputs.at(2) -
                         inputs.at(3)) / config.wheelRadius;
 
     const double bTz = -(config.wheelBase / 2 + config.trackWidth / 2) * (inputs.at(0) -

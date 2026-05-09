2217e9f4df5c18c171284b18d2d80c046f23fd60
West <westly.bouchard@gmail.com>
2026-05-08 18:45:45 -0600

    Build with wasm

--- userCode/ardMain.cpp
+++ userCode/ardMain.cpp
@@ -4,10 +4,18 @@
 
 #include "arduino/ArduinoRuntime.h"
 
+#include "arduino/Motor.h"
+
+Motor m(28);
+
 void setup() {
     pinMode(1, INPUT);
 }
 
 void loop() {
-
+    if (digitalRead(1)) {
+        m.run(255);
+    } else {
+        m.run(0);
+    }
 }
